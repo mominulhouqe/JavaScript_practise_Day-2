22eae398dd86c1e25c666b40eb10f53cbf6e8bdd
Mominul Hoque <88490184+Momin69@users.noreply.github.com>
2023-02-03 12:13:54 +0600

    first commit

--- OddNumber.js
+++ OddNumber.js
@@ -1,30 +1,26 @@
-
 //  Get Odd Numbers of an array and get odd Sum of an array
 
-function OddNumber( Number){
-    let oddStore = [];
-    let sum = 0;
-    for(let i = 0; i < Number.length; i++){
-        const indexNumber = i;
- 
-        const element = Number[i];
+function OddNumber(Number) {
+  let oddStore = [];
+  let sum = 0;
+  for (let i = 0; i < Number.length; i++) {
+    const indexNumber = i;
+    const element = Number[i];
 
-        sum = sum + element;
+    sum = sum + element;
 
-        if(element % 2 !== 0){
-            // console.log(indexNumber , element, sum);
-            oddStore.push(element );
-        }      
-        console.log(indexNumber, oddStore);
+    if (element % 2 !== 0) {
+      // console.log(indexNumber , element, sum);
+      oddStore.push(element);
     }
-    return sum;
+    console.log(indexNumber, oddStore);
+  }
+  return sum;
 }
-const myNumber = [23,43,24,45,56,35,22,40];
+const myNumber = [23, 43, 24, 45, 56, 35, 22, 40];
 
 const addNumber = OddNumber(myNumber);
-console.log("Total Odd Number is = ",addNumber);
+console.log("Total Odd Number is = ", addNumber);
 
 // const SumofOddNumber = OddNumber(addNumber);
 // console.log(SumofOddNumber);
-
-
